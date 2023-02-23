547b235b1b4a426a29350b252e177c94856e44d2
wushuo <2725730722@qq.com>
2023-02-23 21:58:57 +0800

    Update index.js
    修改脚本适用地址为web.telegram.org/k/,因为旧的web.telegram.org/z/ 对贴图的清晰度做了限制;
    支持静态图片自动转换成为png,动态图片仍需自行转换

--- index.js
+++ index.js
@@ -3,33 +3,58 @@
 // @namespace    https://github.com/W2725730722/Telegram-Sticker-Download
 // @version      0.1
 // @author       wushuo
-// @match        https://web.telegram.org/z/
+// @match        https://web.telegram.org/k/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=telegram.org
+// @require      http://jnordberg.github.io/gif.js/gif.js?v=3
 // @grant        none
 // ==/UserScript==
 
 setInterval(() => {
-    let stickerButtonWrapper = document.querySelector('.StickerSetModal .modal-content > .button-wrapper');
-    if (!stickerButtonWrapper) {
+    let ssf = document.querySelector('.popup-body > .sticker-set-footer');
+    if (!ssf) {
         return;
     }
 
-    if (document.querySelector('.StickerSetModal .modal-content > .button-wrapper > #stickerDownloadButton')) {
+    if (document.querySelector('.popup-body > .sticker-set-footer > #stickerDownloadButton')) {
         return;
     }
 
     let downloadButton = document.createElement('button');
     downloadButton.id = 'stickerDownloadButton';
-    downloadButton.className = 'Button smaller primary fluid';
+    downloadButton.className = 'btn-primary btn-color-primary';
     downloadButton.textContent = '贴图下载'
+
+    ssf.append(downloadButton)
+
+
     downloadButton.onclick = () => {
         let index = 0;
-        let stickerButtons = document.querySelectorAll('.stickers .StickerButton');
+        let stickerButtons = document.querySelectorAll('.sticker-set-stickers .sticker-set-sticker');
         for (let v of stickerButtons) {
-            let childNode = v.childNodes[1];
+            let childNode = v.childNodes[0];
             let strings = childNode.src.split('/');
-            let fileName = `${strings[strings.length - 1]}${childNode.tagName === 'IMG' ? '.webp' : '.webm'}`
+            let fileName = `${strings[strings.length - 1]}${childNode.tagName === 'IMG' ? '.png' : '.webm'}`
             setTimeout(() => {
+                if (childNode.tagName === 'IMG') {
+                    const image = new Image();
+                    image.src = childNode.src;
+                    image.onload = function () {
+                        const canvas = document.createElement("canvas");
+                        canvas.width = image.width;
+                        canvas.height = image.height;
+                        canvas.getContext("2d").drawImage(image, 0, 0);
+
+                        const link = document.createElement('a');
+                        link.style.display = 'none';
+                        link.href = canvas.toDataURL('image/png');
+                        link.setAttribute('download', fileName);
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }
+                    return
+                }
+
                 let x = new XMLHttpRequest()
                 x.open('GET', childNode.src, true)
                 x.responseType = 'blob'
@@ -48,9 +73,5 @@ setInterval(() => {
         window.alert(`下载已开始 共${stickerButtons.length}个贴图`)
     };
 
-    stickerButtonWrapper.append(downloadButton)
+
 }, 1000);
-
-
-
-
